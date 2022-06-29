8d8d4a8d7f915e704696e0b10ece3dbe983b7c1d
Andrea-Guevara <montoyaandrea2018@gmail.com>
2022-06-29 20:37:37 -0300

    componente EntradaTexto

---
+++ src/components/EntradaTexto/index.js
@@ -0,0 +1,7 @@
+import { EntradaContainer } from "./styles";
+
+const EntradaTexto = ({ placeholder }) => {
+  return <EntradaContainer placeholder={placeholder} />;
+};
+
+export default EntradaTexto;
