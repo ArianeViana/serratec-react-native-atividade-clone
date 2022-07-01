9a0fa56ed32d929d10c0fcd1b999b8a59804b5c9
douglascorrea95 <douglaspqdt1995@gmail.com>
2022-07-01 15:41:10 -0300

    Tela Inicio

---
+++ src/screens/TelaInicial/index.js
@@ -0,0 +1,93 @@
+import { AntDesign, Feather } from "@expo/vector-icons";
+import {
+  Container,
+  Title,
+  Header,
+  Imagem,
+  EditarLixo,
+  EditarImagem,
+  ImagemCriadores,
+  TituloTexto,
+  TextoLoren,
+  ImagemCategoria,
+  ImagemCentro,
+  Criadores,
+  EditarNomes,
+  NomeCriadores,
+  EditarTecs,
+  TextoTecs,
+} from "./styles.js";
+import { useNavigation } from "@react-navigation/native";
+import { TouchableOpacity, } from "react-native";
+import Gradiente from '../../components/Gradiente/index';
+import Icones from './../../components/Icones/index';
+
+const TelaInicial = () => {
+  const navigation = useNavigation();
+
+  function openScreenHome() {
+    navigation.navigate("Home");
+  }
+
+  return (
+    <Container>
+      <Gradiente posicao="top"/>
+      <Header>
+        <TouchableOpacity>
+          <AntDesign
+            name="logout"
+            size={20}
+            color="black"
+            onPress={openScreenHome}
+          />
+        </TouchableOpacity>
+        <Title>SOCORRO!!!</Title>
+        <Imagem source={require("../../../assets/logo.jpeg")} />
+      </Header>
+        <TituloTexto>Sobre Nós...</TituloTexto>
+        <TextoLoren>Lorem Ipsum is simply dummy text of the printing and</TextoLoren> 
+        <TextoLoren>typesetting industry. Lorem Ipsum has been the industry's</TextoLoren>
+        <TextoLoren>unchanged. It was popularised in the 1960s with the release of</TextoLoren> 
+        <TextoLoren>Letraset sheets containing Lorem Ipsum passages, and more</TextoLoren> 
+        <TextoLoren>recently with desktop publishing software like Aldus PageMaker</TextoLoren>
+        <TextoLoren>including versions of Lorem Ipsum.</TextoLoren>
+        
+        <TituloTexto>Categorias <AntDesign name="plus" size={20} color="#05148c" /></TituloTexto>
+        <EditarLixo>
+        <Feather name="trash" size={20} color="red" />
+        <AntDesign name="edit" size={24} color="black" />
+        </EditarLixo>
+
+        <EditarImagem>
+        <ImagemCategoria source={require("../../../assets/3.jpg")} />
+        <ImagemCentro source={require("../../../assets/5.jpg")} />
+        <ImagemCategoria source={require("../../../assets/1.jpg")} />
+        </EditarImagem>
+        <EditarTecs>
+        <TextoTecs>Tec3</TextoTecs>
+        <TextoTecs>Tec1</TextoTecs>
+        <TextoTecs>Tec2</TextoTecs>
+        </EditarTecs>
+
+        <Criadores>
+        <TituloTexto>Criadores</TituloTexto>
+        <EditarImagem>
+        <ImagemCriadores source={require("../../../assets/People4.jpg")} />
+        <ImagemCriadores source={require("../../../assets/People3.jpg")} />
+        <ImagemCriadores source={require("../../../assets/People2.jpg")} />
+        <ImagemCriadores source={require("../../../assets/People1.jpg")} />
+        </EditarImagem>
+        <EditarNomes>
+            <NomeCriadores>Alex</NomeCriadores>
+            <NomeCriadores>Beatriz</NomeCriadores>
+            <NomeCriadores>Carlos</NomeCriadores>
+            <NomeCriadores>Alie</NomeCriadores>
+        </EditarNomes>
+        </Criadores>
+      <Icones />
+      <Gradiente posicao="bottom"/>
+    </Container>
+  );
+};
+
+export default TelaInicial;
---
+++ src/screens/TelaInicial/styles.js
@@ -0,0 +1,149 @@
+import styled from "styled-components/native";
+import colors from "../../Theme/colors";
+
+export const Container = styled.View`
+  flex: 1;
+  justify-content: space-between;
+`;
+
+export const Header = styled.View`
+  height: 100px;
+  flex-direction: row;
+  justify-content: space-evenly;
+  align-items: center;
+  margin-top: 10px;
+`;
+
+export const Title = styled.Text`
+  font-size: 23;
+  font-weight: bold;
+  color: ${colors.secondary};
+  text-align: center;
+  margin-left: 25px;
+`;
+
+export const Imagem = styled.Image`
+  width: 50px;
+  height: 50px;
+  border-radius: 50px;
+`;
+
+export const InputTexto = styled.TextInput`
+  width: 100%;
+  height: 35px;
+  color: ${colors.secondary};
+  font-size: 15;
+  padding: 10px;
+  width: 60%;
+  background-color: ${colors.tertiary};
+  border-radius: 10px;
+  border: 1px solid;
+  border-color: ${colors.primary};
+  height: 35px;
+  align-self: center;
+  box-shadow: 0px 5px 5px #2f4f4f;
+`;
+
+
+export const TextoSelecionarImagem = styled.Text`
+  font-size: 13;
+  align-self: center;
+  color: ${colors.secondary};
+`;
+
+export const IconeTexto = styled.TouchableOpacity`
+  margin-left: 3px;
+`;
+
+export const BotaoCadastrar = styled.TouchableOpacity`
+  width: 120px;
+  height: 30px;
+  background-color: ${colors.tertiary};
+  align-self: center;
+  margin-top: 25px;
+  border-radius: 10px;
+  border: 1px solid;
+  justify-content: center;
+  align-items: center;
+  border-color: ${colors.primary};
+  box-shadow: 0px 5px 5px #2f4f4f;
+`;
+
+export const TextoBotaoCadastrar = styled.Text`
+  color: #05148c;
+  font-weight: bold;
+`;
+
+export const EditarLixo = styled.View`
+  flex-direction: row;
+  justify-content: flex-end;
+  width: 75%;
+`;
+
+export const EditarImagem = styled.View`
+  flex-direction: row;
+  justify-content: space-evenly;
+`;
+
+export const ImagemCriadores = styled.Image`
+  width: 80px;
+  height: 80px;
+  border-radius: 100px;
+  border: 3px solid;
+  border-color: #05148c;
+`;
+
+export const TituloTexto = styled.Text`
+  color: ${colors.secondary};
+  font-size: 25;
+  margin: 20px;
+`;
+
+export const TextoLoren = styled.Text`
+  font-size: 10;
+  text-align: center;
+  font-weight: bold;
+  align-self: center;
+`;
+
+export const ImagemCategoria = styled.Image`
+  width: 70px;
+  height: 100px;
+  border-radius: 10px;
+  margin-top: 60px;
+`;
+
+export const ImagemCentro = styled.Image`
+  width: 210px;
+  height: 160px;
+  border-radius: 10px;
+  `;
+
+export const Criadores = styled.View`
+  /* margin-top: 150px; */
+`;
+
+export const EditarNomes = styled.View`
+  flex-direction: row;
+  justify-content: space-evenly;
+`;
+
+export const NomeCriadores = styled.Text`
+    color: #05148c;
+    margin: 3px;
+    padding: 10px;
+    font-weight: bold;
+`;
+
+export const EditarTecs = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  width: 85%;
+  margin-left: 30px;
+`;
+
+export const TextoTecs = styled.Text`
+  color: #05148c;
+  font-weight: bold;
+`;
+
